e31289e4d69202a61863f9e5d32331811586bae7
sid2417 <siddhartha.java9398@gmail.com>
2024-07-10 02:48:22 +0530

    Installing DB server

---
+++ backend.sh
@@ -0,0 +1,93 @@
+echo "Enter your Password : "
+read DB_Password
+
+#colors
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+#Date #ScriptName #Logfile
+TIME_STAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo "$?" | cut -d "." -f1)
+LOG_FILE=/tmp/$SCRIPT_NAME+$TIME_STAMP.log
+
+
+
+#UserId
+USER_ID=$(id -u)
+if [ $USER_ID -ne 0 ]
+then 
+    echo -e "$R Please Provide SUDO access...$N"
+    exit 1
+else    
+    echo -e "$G You are a Super user $N"
+fi
+
+
+VALIDATE ()
+{
+    if [ $1 -ne 0 ]
+    then 
+        echo -e "$R $2 FAILURE $N"
+        exit 1
+    else    
+        echo -e "$G $2 SUCCESS $N"
+    fi
+}
+
+
+dnf module disable nodejs -y
+VALIDATE $? "Disabling default nodejs"
+
+dnf module enable nodejs:20 -y
+VALIDATE $? "Enabling nodejs:20 version"
+
+dnf install nodejs -y
+VALIDATE $? "Installing nodejs"
+
+id expense &>>$LOGFILE
+if [ $? -ne 0 ]
+then
+    useradd expense &>>$LOGFILE
+    VALIDATE $? "Creating expense user"
+else
+    echo -e "Expense user already created...$Y SKIPPING $N"
+fi
+
+mkdir -p /app &>>$LOGFILE
+VALIDATE $? "Creating app directory"
+
+curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip
+VALIDATE $? "Downloading backend code"
+
+
+cd /app &>>$LOGFILE
+rm -rf /app/* &>>$LOGFILE
+
+unzip /tmp/backend.zip &>>$LOGFILE
+VALIDATE $? "Extracted backend code"
+
+npm install &>>$LOGFILE
+VALIDATE $? "Installing nodejs dependencies"
+
+cp /home/ec2-user/Installation/backend.service /etc/systemd/system/backend.service &>>$LOGFILE
+VALIDATE $? "Copied backend service"
+
+systemctl daemon-reload &>>$LOGFILE
+VALIDATE $? "Daemon Reload"
+
+systemctl start backend &>>$LOGFILE
+VALIDATE $? "Starting backend"
+
+systemctl enable backend &>>$LOGFILE
+VALIDATE $? "Enabling backend"
+
+dnf install mysql -y &>>$LOGFILE
+VALIDATE $? "Installing MySQL Client"
+
+mysql -h db.happywithyogamoney.fun -uroot -p${DB_Password} < /app/schema/backend.sql &>>$LOGFILE
+VALIDATE $? "Schema loading"
+
+systemctl restart backend &>>$LOGFILE
+VALIDATE $? "Restarting Backend"
